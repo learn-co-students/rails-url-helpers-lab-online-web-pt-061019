e5a8951c33fb802641060cd309e398ba398b48e1
scrabill <shannon.crabill@gmail.com>
2019-10-20 15:49:57 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+
+  #The get, is the URL path
+  # to: calls on the activate method within the Students controller
+  # as: ???
+
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+
 end
