485c7d5b33f297f99ab3964d86a0904ef6272d67
maasa333 <konomaasa@gmail.com>
2019-10-20 19:02:05 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,12 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+    if @student.active == true
+      puts "This student is currently active."
+    else
+      puts "This student is currently inactive."
+    end
   end
 
   private
