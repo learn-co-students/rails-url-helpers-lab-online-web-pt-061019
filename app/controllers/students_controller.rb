b99752be729a94e6cbba1ba0b231559ac41a8192
ZumbaIdol <david.a.foote@gmail.com>
2019-10-24 14:58:13 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
     @student.active = !@student.active
     @student.save
 
-    redirect student_path(@student)
+    redirect_to student_path(@student)
   end
 
   private
