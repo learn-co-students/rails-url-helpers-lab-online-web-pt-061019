307ea59c0ae32f919683e772f15f89a019448047
Jack <jackrrye@gmail.com>
2019-10-24 20:32:27 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,10 +3,21 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
-  end
+  end 
 
   def show
   end
+
+  def activate
+    student = Student.find(params[:id])
+    if student.active == false
+      student.active = true
+    else 
+      student.active = false
+    end 
+    student.save
+    redirect_to student_url
+  end 
 
   private
 
